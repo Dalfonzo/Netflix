5e7e0024db288a9bdf00c33d8894b5a80f6f1f6b
dap <dalfonzo18@gmail.com>
2020-02-05 12:36:52 -0400

    Project finished

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,12 +3,14 @@ import "./css/App.css";
 import { Switch, Route } from "react-router-dom";
 import Main from "./pages/index";
 import Login from "./pages/Login";
+import _404 from "./pages/_404";
 
 function App() {
     return (
         <Switch>
             <Route exact path="/" component={Main} />
             <Route path="/login" component={Login} />
+            <Route component={_404} />
         </Switch>
     );
 }
